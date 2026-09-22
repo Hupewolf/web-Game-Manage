d49e60957e907bc16f6a61bed3acbf16434c6dc6
Hupewolf <nhannguyen.ix5@gmail.com>
2026-09-23 04:33:33 +0700

    done res

--- pages/room/home.js
+++ pages/room/home.js
@@ -1,23 +1,71 @@
 import { playerState, GameHeader } from '../../share/main.js';
 import { DashboardPanel } from '../../component/dashboardPanel/dashboardPanel.js';
 
+// function resizeGame() {
+//     const game = document.querySelector("#web");
+
+//     const scaleX = window.innerWidth / 1920;
+//     const scaleY = window.innerHeight / 1080;
+
+//     const scale = Math.min(scaleX, scaleY);
+
+//     const x = (window.innerWidth - 1920 * scale) / 2;
+//     const y = (window.innerHeight - 1080 * scale) / 2;
+
+//     game.style.transform =
+//         `translate(${x}px, ${y}px) scale(${scale})`;
+// }
+
+
+// window.addEventListener("resize", resizeGame);
+
+// resizeGame();
+
+
+
 function resizeGame() {
     const game = document.querySelector("#web");
 
-    const scaleX = window.innerWidth / 1920;
-    const scaleY = window.innerHeight / 1080;
+    const viewportWidth = window.innerWidth;
+    const viewportHeight = window.innerHeight;
+
+    const designWidth = 1920;
+    const designHeight = 1080;
+
+    let baseWidth = designWidth;
+    let baseHeight = designHeight;
+
+    const normalHeight = viewportWidth * 9 / 16;
+
+    // Browser làm viewport thấp hơn tỷ lệ 16:9
+    if (viewportHeight < normalHeight) {
+
+        const missing = normalHeight - viewportHeight;
+
+        baseWidth = designWidth + missing * 2;
+        // Giảm chiều cao base
+        baseHeight = designHeight - missing;
+
+        // Không cho baseHeight quá nhỏ
+        baseHeight = Math.max(baseHeight, 900);
+    }
+
+    const scaleX = viewportWidth / baseWidth;
+    const scaleY = viewportHeight / baseHeight;
 
     const scale = Math.min(scaleX, scaleY);
 
-    const x = (window.innerWidth - 1920 * scale) / 2;
-    const y = (window.innerHeight - 1080 * scale) / 2;
+    const x = (viewportWidth - baseWidth * scale) / 2;
+    const y = (viewportHeight - baseHeight * scale) / 2;
+
+    game.style.width = `${baseWidth}px`;
+    game.style.height = `${baseHeight}px`;
 
     game.style.transform =
         `translate(${x}px, ${y}px) scale(${scale})`;
 }
 
 window.addEventListener("resize", resizeGame);
-
 resizeGame();
 
 // Bảng quản lý iPad — render sẵn nhưng ở trạng thái ĐÓNG.
